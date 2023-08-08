44ee31a3040f220578a116d6cd34102ad0880af3
Marlon (Mj) Burog <spicyrice09@gmail.com>
2023-08-08 01:39:21 -0700

    Update tristan.sh

--- tristan.sh
+++ tristan.sh
@@ -1,6 +1,13 @@
+cd Documents
+
+mkdir coding
+
+cd coding
+
 sudo apt-get update && sudo apt-get full-upgrade -y
 
 sudo apt-get install nodejs npm git -y
 
 sudo npm install yarn --global
 
+sudo yarn create react-app ramTristan
